ece45ce61893469b7c0aaa07ef6a379f62ec347e
Fe2O3-Tpa <gogottu1221@gmail.com>
2025-11-28 22:28:40 +0900

    テンプレートを更新。

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,9 @@ fn read_buffer_vec() -> Vec<String> {
     io::stdin().read_line(&mut buffer).expect("Failed to read line.");
     buffer.trim().split(" ").map(|s| s.to_string()).collect()
 }
+
+fn read_buffer() -> String {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("Failed to read line.");
+    buffer.trim().to_string()
+}
